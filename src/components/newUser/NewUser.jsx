a35c09a30af9734221f8765fa0cec9a44abf74a8
fahim <basitabdullah1122@gmail.com>
2024-01-01 16:47:32 +0530

    fixed bugs

--- src/components/newUser/NewUser.jsx
+++ src/components/newUser/NewUser.jsx
@@ -4,74 +4,83 @@ import Sidebar from "../sidebar/Sidebar";
 import { doc, serverTimestamp, setDoc } from "firebase/firestore"; 
 import { userInputs } from "../../inputsData";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import {db, storage } from "../../firebase"
+import {auth, db, storage } from "../../firebase"
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
 
-const NewUser = () => {
-  const [file, setFile] = useState("");
-  const [data,setData] = useState({})
-  const [per, setPerc] = useState(null);
 
-  useEffect(() => {
-    const uploadFile = () => {
-      const name = new Date().getTime() + file.name;
-
-      console.log(name);
-      const storageRef = ref(storage, file.name);
-      const uploadTask = uploadBytesResumable(storageRef, file);
-
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          setPerc(progress);
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-            default:
-              break;
+  const NewUser = ({  label }) => {
+    const [file, setFile] = useState("");
+    const [data, setData] = useState({});
+    const [per, setPerc] = useState(null);
+    const navigate = useNavigate()
+  
+    useEffect(() => {
+      const uploadFile = () => {
+        const name = new Date().getTime() + file.name;
+  
+        console.log(name);
+        const storageRef = ref(storage, file.name);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+  
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            setPerc(progress);
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {
+            console.log(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              setData((prev) => ({ ...prev, img: downloadURL }));
+            });
           }
-        },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            setData((prev) => ({ ...prev, img: downloadURL }));
-          });
-        }
-      );
+        );
+      };
+      file && uploadFile();
+    }, [file]);
+  
+    console.log(data);
+  
+    const handleInput = (e) => {
+      const id = e.target.id;
+      const value = e.target.value;
+  
+      setData({ ...data, [id]: value });
     };
-    file && uploadFile();
-  }, [file]);
-
- 
-  const handleInput = (e) => {
-    const id = e.target.id  
-    const value = e.target.value
-
-    setData({...data, [id] : value})
-
-
-    
-  };
-  const handleSubmit = async(e) => {
-    e.preventDefault();
-
-   const res = await createUserWithEmailAndPassword(data.email,data.password)
-    await setDoc(doc(db, "users", res.user.uid), {
-     ...data,
-     timeStamp : serverTimestamp()
-    });
-
-    
-  };
+  
+    const handleAdd = async (e) => {
+      e.preventDefault();
+      try {
+        const res = await createUserWithEmailAndPassword(
+          auth,
+          data.email,
+          data.password
+        );
+        await setDoc(doc(db, "users", res.user.uid), {
+          ...data,
+          timeStamp: serverTimestamp(),
+        });
+        navigate(-1)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+  
 
   return (
     <div className="newUser">
@@ -80,7 +89,7 @@ const NewUser = () => {
         {/* <Navbar /> */}
         <div className="formContainer">
           <h1>ADD USER</h1>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleAdd}>
             <input
               type="file"
               id="fileAdd"
@@ -108,7 +117,7 @@ const NewUser = () => {
                 />
               </label>
             ))}
-            <button type="submit">SUBMIT</button>
+            <button disabled={per !== null && per <100 } type="submit">SUBMIT</button>
           </form>
         </div>
       </div>
